d18c2cd27cddc6762e37d253bbad79060bf9db8e
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-11-27 19:02:42 -0300

    Funções construtoras e prototype

---
+++ aulajs/funcoes-contrutoras-e-prototypes/script.js
@@ -0,0 +1,38 @@
+const p1 = {
+    name: "Computador",
+    price: 3000.0,
+    quantity: 2
+}
+
+//FUNÇÃO CONSTRUTORA
+const Product = function(name, price, quantity){
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+
+    this.label = function(){
+        return "Dados (funcao interna): " + this.name + ", " + this.price.toFixed(2) + ", " + this.quantity;
+    }
+}
+
+Product.prototype.total = function(){
+    return this.price * this.quantity;
+}
+
+Product.prototype.add = function(amount){
+    this.quantity += amount;
+}
+
+Product.prototype.remove = function(amount){
+    if(this.quantity >= amount){
+        this.quantity -= amount;
+    }
+}
+
+Product.prototype.label = function(){
+    return "Dados: " + this.name + ", " + this.price.toFixed(2) + ", " + this.quantity;
+}
+
+const p2 = new Product("Monitor", 800.0, 10);
+
+const p3 = new Product("Mouse", 50.0, 4);
