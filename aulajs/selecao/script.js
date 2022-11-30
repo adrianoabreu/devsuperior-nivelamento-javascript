124ccca9abb70d6f3797e63c024ce8b533cef63a
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-11-30 18:47:09 -0300

    Seleção de elementos

---
+++ aulajs/selecao/script.js
@@ -0,0 +1,29 @@
+const item1 = document.querySelector("li");
+
+console.log(item1);
+console.log(item1.innerHTML);
+console.log(item1.outerHTML);
+
+const items = document.querySelectorAll("li");
+
+console.log(items);
+
+const card1 = document.querySelector(".card");
+
+console.log(card1);
+
+const paragrafo = document.createElement("p");
+paragrafo.innerHTML = "Descrição";
+card1.appendChild(paragrafo);
+
+card1.classList.add("super-border");
+
+const cards = document.querySelectorAll(".card");
+//const cards = document.getElementsByClassName("card");
+
+console.log(cards);
+
+Array.from(cards).map(item => {
+    console.log(item.innerHTML);
+});
+
