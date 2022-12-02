c2c0bdeaa5a17169983243ad13cf72f0f13d159f
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-12-01 22:41:52 -0300

    Criando a função construtora Address

--- projeto-viacep/js/models/address.js
+++ projeto-viacep/js/models/address.js
@@ -1,4 +1,4 @@
-export default function Address(cep, street, number city){
+export default function Address(cep, street, number, city){
     this.cep    = cep;
     this.street = street;
     this.number = number;
