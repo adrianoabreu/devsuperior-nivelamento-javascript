d0886f1d131f56945958d430f95162e5e2686d66
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-12-01 22:34:22 -0300

    Selecionando elementos do formulário

--- projeto-viacep/js/controllers/form-controller.js
+++ projeto-viacep/js/controllers/form-controller.js
@@ -1,7 +1,31 @@
 
+function State(){
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+    
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
+
 export function init(){
 
-    const inputCep = document.forms.newAddress.cep;
+    state.inputCep    = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity   = document.forms.newAddress.city;
 
-    console.log(inputCep);
+    state.btnSave     = document.forms.newAddress.btnSave;
+    state.btnClear    = document.forms.newAddress.btnClear;
+
+    state.errorCep    = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
+
+    console.log(state);
 }
