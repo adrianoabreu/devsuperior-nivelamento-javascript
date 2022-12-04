eb533f13c602a8f607a5b9f8c90b9cd9ecddd74b
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-12-04 11:44:32 -0300

    Evento handleInputCepChange

--- projeto-viacep/js/controllers/form-controller.js
+++ projeto-viacep/js/controllers/form-controller.js
@@ -1,5 +1,5 @@
 import Address from '../models/address.js';
-import * as requestService from '../services/request-service.js';
+import * as addressService from '../services/address-service.js';
 
 function State(){
 
@@ -37,13 +37,36 @@ export function init(){
     state.inputNumber.addEventListener('change', handleInputNumberChange);
     state.btnClear.addEventListener('click', handleBtnClearClick);
     state.btnSave.addEventListener('click', handleBtnSaveClick);
+    state.inputCep.addEventListener('change', handleInputCepChange);
+
+}
+
+async function handleInputCepChange(event) {
+    //event.preventDefault();
+    const cep = event.target.value;
+
+    try {
+        const address = await addressService.findByCep(cep);
+
+        state.inputStreet.value = address.street;
+        state.inputCity.value = address.city;
+        state.address = address;
+
+        setFormError("cep", "");
+        state.inputNumber.focus();
+    }
+    catch (e) {
+        state.inputStreet.value = "";
+        state.inputCity.value = "";
+        setFormError("cep", "Informe um CEP válido");
+    }
 
 }
 
 async function handleBtnSaveClick(event) {
     event.preventDefault();
-    const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
-    console.log(result);
+    //const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
+    console.log(event.target);
 }
 
 function handleInputNumberChange(event) {
